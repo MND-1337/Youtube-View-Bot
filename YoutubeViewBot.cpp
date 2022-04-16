b09e14e3be25f9cebefafad8f2e9604ff67ac0d7
MND1337 <103332176+MND-1337@users.noreply.github.com>
2022-04-16 17:49:55 +0000

    Update YoutubeViewBot.cpp

--- YoutubeViewBot.cpp
+++ YoutubeViewBot.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+#include <windows.h>
+
+
+void urlfromvideo()
+{
+    system("start https://www.youtube.com/watch?v=ZRSAYoEVfrw");
+}
+int main()
+{
+    std::cout << "Youtube Tutorial #1 Code (viewbot)!\n";
+    Sleep(2000);
+    urlfromvideo();
+    Sleep(15000); //15 is working :)
+    system("taskkill /IM Chrome.exe /F"); //thats kill chrome after 25 secónds
+    system("cls");
+    main();
+}
+
+//this is the video i hope you enjoy the bot leave a like and subscribe
